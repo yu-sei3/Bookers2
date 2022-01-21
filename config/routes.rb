9d3d64689fdeb6ac4f8b93574e9f60565542c2fd
内田勇征 <96036914+yu-sei3@users.noreply.github.com>
2022-01-21 13:01:07 +0000

    [add]headerレイアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
   devise_for :users
+
+  root to: "homes#top"
+  get 'homes/about' => 'homes#about'
+
+  resources :users, only: [:new, :index, :show, :edit]
+
+  resources :books, only: [:index, :show, :edit]
 
 end
